9d8d2f44785260e4b856eb9b86297342d65e21db
Nguyễn Phương Thảo <thaonphe182154@fpt.edu.vn>
2025-06-18 17:11:00 +0700

    update detail comic js
    add list chapter

--- src/containers/detail.js
+++ src/containers/detail.js
@@ -94,7 +94,7 @@ function DetailComic() {
     <>
       <Header />
       <div className="pt-[115px] px-4 pb-10">
-        <div className="max-w-7xl mx-auto mt-4 bg-white rounded-2xl shadow-xl p-6 flex flex-col md:flex-row gap-8">
+        <div className="max-w-7xl mx-auto mt-4 bg-white rounded-2xl shadow-md p-6 flex flex-col md:flex-row gap-8">
           {/* Ảnh bìa */}
           <div className="flex-shrink-0 mx-auto md:mx-0">
             <img
@@ -154,7 +154,7 @@ function DetailComic() {
           </div>
         </div>
         {/* Mô tả giới thiệu truyện */}
-        <div className="max-w-7xl mx-auto mt-4 bg-white rounded-2xl shadow-xl p-6 ">
+        <div className="max-w-7xl mx-auto mt-4 bg-white rounded-2xl shadow-md p-6 ">
           <h3 className="text-xl font-semibold mb-2">Giới thiệu:</h3>
           <p className="text-gray-700">
             Đây là một bộ truyện tranh hấp dẫn với cốt truyện độc đáo và hình
@@ -186,15 +186,17 @@ function DetailComic() {
             </button>
           </div>
         </div>
-        <div className="mt-4 max-w-7xl mx-auto rounded-2xl shadow-xl p-6">
-          <div className="flex items-center justify-between mb-4">
-            <h2 className="bg-gradient-to-r from-indigo-500 to-sky-300 bg-clip-text text-transparent font-bold flex items-center gap-2 mb-4">
-              <svg
+        {/* Danh sách truyện */}
+        <div className="mt-4 max-w-7xl mx-auto rounded-2xl shadow-md p-6">
+          <div className="grid grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 gap-2">
+              <div className="h-full text-lg uppercase bg-gradient-to-r from-indigo-700 to-violet-500 bg-clip-text text-transparent font-bold flex items-center gap-2 mb-4">
+                <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
-                stroke="#7ccefb"
+                stroke="#4f3ed2"
                 className="size-6"
               >
                 <path
@@ -203,55 +205,59 @@ function DetailComic() {
                   d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
                 />
               </svg>
-              DANH SÁCH CHƯƠNG
-            </h2>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="#7ccefb"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="#7ccefb"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="#7ccefb"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
-              />
-            </svg>
+              <span>Danh sách chương</span>
+                
+              </div>
+            </div>
+            <div className="grid grid-cols-3 gap-4 justify-items-center items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#4f3ed2"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#4f3ed2"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#4f3ed2"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
